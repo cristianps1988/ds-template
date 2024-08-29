cdc0a30ee750d693bb44049f0ef78808ed951dad
Cristianps1988 <cristianps_1@hotmail.com>
2024-08-29 18:19:55 -0500

    rollup css export config added

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,427 @@
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import terser from '@rollup/plugin-terser';
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import { babel } from '@rollup/plugin-babel';
+// import postcss from 'rollup-plugin-postcss';
+// import typescript from '@rollup/plugin-typescript';
+// import dts from 'rollup-plugin-dts';
+// import postcssImport from 'postcss-import';
+// import fs from 'fs';
+// import path from 'path';
+
+// import { createRequire } from 'node:module';
+// const requireFile = createRequire(import.meta.url);
+// const packageJson = requireFile('./package.json');
+
+// export default [
+//   {
+//     input: 'src/index.ts',
+//     output: [
+//       {
+//         file: packageJson.main,
+//         format: 'cjs',
+//         sourcemap: true,
+//       },
+//       {
+//         file: packageJson.module,
+//         format: 'esm',
+//         exports: 'named',
+//         sourcemap: true,
+//       },
+//     ],
+//     plugins: [
+//       postcss({
+//         extensions: ['.css'],
+//         extract: true,
+//         minimize: true,
+//         plugins: [require('postcss-import')()],
+//       }),
+//       peerDepsExternal(),
+//       resolve({
+//         extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//       }),
+//       commonjs(),
+//       typescript({ tsconfig: './tsconfig.json' }),
+//       terser(),
+//       babel({
+//         extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//         exclude: 'node_modules/**',
+//         babelHelpers: 'bundled',
+//       }),
+//     ],
+//     external: ['react', 'react-dom'],
+//   },
+//   {
+//     input: 'dist/esm/types/index.d.ts',
+//     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+//     plugins: [dts.default()],
+//     external: [/\.css$/],
+//   },
+// ];
+
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import terser from '@rollup/plugin-terser';
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import { babel } from '@rollup/plugin-babel';
+// import postcss from 'rollup-plugin-postcss';
+// import typescript from '@rollup/plugin-typescript';
+// import dts from 'rollup-plugin-dts';
+// import postcssImport from 'postcss-import';
+// import { createRequire } from 'node:module';
+// const requireFile = createRequire(import.meta.url);
+// const packageJson = requireFile('./package.json');
+
+// export default [
+//   {
+//     input: 'src/index.ts',
+//     output: [
+//       {
+//         file: packageJson.main,
+//         format: 'cjs',
+//         sourcemap: true,
+//       },
+//       {
+//         file: packageJson.module,
+//         format: 'esm',
+//         exports: 'named',
+//         sourcemap: true,
+//       },
+//     ],
+//     plugins: [
+//       postcss({
+//         extensions: ['.css'],
+//         extract: 'styles.css',
+//         minimize: true,
+//         plugins: [postcssImport()],
+//       }),
+//       peerDepsExternal(),
+//       resolve({
+//         extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//       }),
+//       commonjs(),
+//       typescript({ tsconfig: './tsconfig.json' }),
+//       terser(),
+//       babel({
+//         extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//         exclude: 'node_modules/**',
+//         babelHelpers: 'bundled',
+//       }),
+//     ],
+//     external: ['react', 'react-dom'],
+//   },
+//   {
+//     input: 'dist/esm/types/index.d.ts',
+//     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+//     plugins: [dts.default()],
+//     external: [/\.css$/],
+//   },
+// ];
+
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import terser from '@rollup/plugin-terser';
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import { babel } from '@rollup/plugin-babel';
+// import postcss from 'rollup-plugin-postcss';
+// import typescript from '@rollup/plugin-typescript';
+// import dts from 'rollup-plugin-dts';
+// import postcssImport from 'postcss-import';
+// import { createRequire } from 'node:module';
+// const requireFile = createRequire(import.meta.url);
+// const packageJson = requireFile('./package.json');
+
+// export default [
+//   {
+//     input: 'src/index.ts',
+//     output: [
+//       {
+//         file: packageJson.main,
+//         format: 'cjs',
+//         sourcemap: true,
+//       },
+//       {
+//         file: packageJson.module,
+//         format: 'esm',
+//         exports: 'named',
+//         sourcemap: true,
+//       },
+//     ],
+//     plugins: [
+//       peerDepsExternal(),
+//       resolve({
+//         extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//       }),
+//       commonjs(),
+//       typescript({ tsconfig: './tsconfig.json' }),
+//       postcss({
+//         extensions: ['.css'],
+//         extract: 'styles.css', // Cambiamos a 'styles.css' en lugar de 'dist/styles.css'
+//         minimize: true,
+//         plugins: [postcssImport()],
+//       }),
+//       terser(),
+//       babel({
+//         extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//         exclude: 'node_modules/**',
+//         babelHelpers: 'bundled',
+//       }),
+//     ],
+//     external: ['react', 'react-dom'],
+//   },
+//   {
+//     input: 'dist/esm/types/index.d.ts',
+//     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+//     plugins: [dts.default()],
+//     external: [/\.css$/],
+//   },
+// ];
+
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import terser from '@rollup/plugin-terser';
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import { babel } from '@rollup/plugin-babel';
+// import postcss from 'rollup-plugin-postcss';
+// import typescript from '@rollup/plugin-typescript';
+// import dts from 'rollup-plugin-dts';
+// import postcssImport from 'postcss-import';
+// import { createRequire } from 'node:module';
+
+// const requireFile = createRequire(import.meta.url);
+// const packageJson = requireFile('./package.json');
+
+// export default [
+//   {
+//     input: 'src/index.ts',
+//     output: [
+//       {
+//         file: packageJson.main,
+//         format: 'cjs',
+//         sourcemap: true,
+//       },
+//       {
+//         file: packageJson.module,
+//         format: 'esm',
+//         exports: 'named',
+//         sourcemap: true,
+//       },
+//     ],
+//     plugins: [
+//       peerDepsExternal(),
+//       resolve({ extensions: ['.ts', '.tsx', '.js', '.jsx'] }),
+//       commonjs(),
+//       typescript({ tsconfig: './tsconfig.json' }),
+//       postcss({
+//         extensions: ['.css'],
+//         extract: 'styles.css',
+//         minimize: true,
+//         plugins: [postcssImport()],
+//       }),
+//       terser(),
+//       babel({
+//         extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//         exclude: 'node_modules/**',
+//         babelHelpers: 'bundled',
+//       }),
+//     ],
+//     external: ['react', 'react-dom'],
+//   },
+//   {
+//     input: 'dist/esm/types/index.d.ts',
+//     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+//     plugins: [dts.default()],
+//     external: [/\.css$/],
+//   },
+// ];
+
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import terser from '@rollup/plugin-terser';
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import { babel } from '@rollup/plugin-babel';
+// import postcss from 'rollup-plugin-postcss';
+// import typescript from '@rollup/plugin-typescript';
+// import dts from 'rollup-plugin-dts';
+// import postcssImport from 'postcss-import';
+// import { createRequire } from 'node:module';
+
+// const requireFile = createRequire(import.meta.url);
+// const packageJson = requireFile('./package.json');
+
+// export default [
+//   {
+//     input: 'src/index.ts',
+//     output: [
+//       {
+//         file: packageJson.main,
+//         format: 'cjs',
+//         sourcemap: true,
+//       },
+//       {
+//         file: packageJson.module,
+//         format: 'esm',
+//         exports: 'named',
+//         sourcemap: true,
+//       },
+//     ],
+//     plugins: [
+//       peerDepsExternal(),
+//       resolve({ extensions: ['.ts', '.tsx', '.js', '.jsx'] }),
+//       commonjs(),
+//       postcss({
+//         extract: true,
+//         modules: false,
+//         plugins: [postcssImport()],
+//       }),
+//       typescript({ tsconfig: './tsconfig.json' }),
+//       terser(),
+//       babel({
+//         extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//         exclude: 'node_modules/**',
+//         babelHelpers: 'bundled',
+//       }),
+//     ],
+//     external: ['react', 'react-dom'],
+//   },
+//   {
+//     input: 'dist/esm/types/index.d.ts',
+//     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+//     plugins: [dts.default()],
+//     external: [/\.css$/],
+//   },
+// ];
+
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import terser from '@rollup/plugin-terser';
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import { babel } from '@rollup/plugin-babel';
+// import postcss from 'rollup-plugin-postcss';
+// import typescript from '@rollup/plugin-typescript';
+// import dts from 'rollup-plugin-dts';
+// import postcssImport from 'postcss-import';
+// import { createRequire } from 'node:module';
+
+// const requireFile = createRequire(import.meta.url);
+// const packageJson = requireFile('./package.json');
+
+// export default [
+//   {
+//     input: 'src/index.ts',
+//     output: [
+//       {
+//         file: packageJson.main,
+//         format: 'cjs',
+//         sourcemap: true,
+//       },
+//       {
+//         file: packageJson.module,
+//         format: 'esm',
+//         exports: 'named',
+//         sourcemap: true,
+//       },
+//     ],
+//     plugins: [
+//       peerDepsExternal(),
+//       resolve({ extensions: ['.ts', '.tsx', '.js', '.jsx'] }),
+//       commonjs(),
+//       postcss({
+//         extract: 'styles.css',
+//         modules: false,
+//         autoModules: false,
+//         minimize: true,
+//         plugins: [postcssImport()],
+//       }),
+//       typescript({ tsconfig: './tsconfig.json' }),
+//       terser(),
+//       babel({
+//         extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//         exclude: 'node_modules/**',
+//         babelHelpers: 'bundled',
+//       }),
+//     ],
+//     external: ['react', 'react-dom'],
+//   },
+//   {
+//     input: 'dist/esm/types/index.d.ts',
+//     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+//     plugins: [dts.default()],
+//     external: [/\.css$/],
+//   },
+// ];
+
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import terser from '@rollup/plugin-terser';
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import { babel } from '@rollup/plugin-babel';
+// import postcss from 'rollup-plugin-postcss';
+// import typescript from '@rollup/plugin-typescript';
+// import dts from 'rollup-plugin-dts';
+// import postcssImport from 'postcss-import';
+// import { createRequire } from 'node:module';
+
+// const requireFile = createRequire(import.meta.url);
+// const packageJson = requireFile('./package.json');
+
+// export default [
+//   {
+//     input: 'src/index.ts',
+//     output: [
+//       {
+//         file: packageJson.main,
+//         format: 'cjs',
+//         sourcemap: true,
+//       },
+//       {
+//         file: packageJson.module,
+//         format: 'esm',
+//         exports: 'named',
+//         sourcemap: true,
+//       },
+//     ],
+//     plugins: [
+//       peerDepsExternal(),
+//       resolve({ extensions: ['.ts', '.tsx', '.js', '.jsx'] }),
+//       commonjs(),
+//       postcss({
+//         extract: 'dist/styles.css',
+//         modules: false,
+//         autoModules: false,
+//         minimize: true,
+//         plugins: [postcssImport()],
+//       }),
+//       typescript({ tsconfig: './tsconfig.json' }),
+//       terser(),
+//       babel({
+//         extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//         exclude: 'node_modules/**',
+//         babelHelpers: 'bundled',
+//       }),
+//     ],
+//     external: ['react', 'react-dom'],
+//   },
+//   {
+//     input: 'dist/esm/types/index.d.ts',
+//     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+//     plugins: [dts.default()],
+//     external: [/\.css$/],
+//   },
+// ];
+
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { babel } from '@rollup/plugin-babel';
-import css from "rollup-plugin-import-css";
+import postcss from 'rollup-plugin-postcss';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
-
-// This is required to read package.json file when
-// using Native ES modules in Node.js
-// https://rollupjs.org/command-line-interface/#importing-package-json
+import postcssImport from 'postcss-import';
 import { createRequire } from 'node:module';
+
 const requireFile = createRequire(import.meta.url);
 const packageJson = requireFile('./package.json');
 
@@ -31,14 +442,16 @@ export default [
       },
     ],
     plugins: [
-      css({
-        output: 'dist/styles.css',
-      }),
       peerDepsExternal(),
-      resolve({
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-      }),
+      resolve({ extensions: ['.ts', '.tsx', '.js', '.jsx'] }),
       commonjs(),
+      postcss({
+        extract: true,
+        modules: false,
+        inject: false,
+        minimize: true,
+        plugins: [postcssImport()],
+      }),
       typescript({ tsconfig: './tsconfig.json' }),
       terser(),
       babel({
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import './styles/global.css';
-import './styles/tokens.css';
-import './styles/reset.css';
+import './styles/index.css';
 
 export * from './Button';
 
-// Exportar los estilos individualmente si es necesario
-export { default as globalStyles } from './styles/global.css';
-export { default as tokenStyles } from './styles/tokens.css';
-export { default as resetStyles } from './styles/reset.css';
+export { default as styles } from './styles/index.css';
